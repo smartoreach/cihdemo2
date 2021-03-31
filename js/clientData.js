83d4629a09a66056cb670b7ebb171f2ca56b5e30
smartoreach <76698713+smartoreach@users.noreply.github.com>
2021-03-31 21:30:32 +0530

    Add files via upload

---
+++ js/clientData.js
@@ -0,0 +1,118 @@
+// Client data
+
+export const ClientData = {
+    clientID : "A01", 
+    domain : "localhost:3000",
+    nav : {
+        sectionList : [
+            {
+                sectionName : "Home",
+                sectionLink : "/"
+            },
+            {
+                sectionName : "About",
+                sectionLink : "/about"
+            },
+            {
+                sectionName : "Products and Services",
+                sectionLink : "/products-and-services"
+            },
+            {
+                sectionName : "Contact us",
+                sectionLink : "/contact"
+            },
+            // {
+            //     sectionName : "Resources",
+            //     sectionLink : "/resources"
+            // }
+        ],
+        logo : {
+            logoUrl : "/images/logo.webp",
+            logoAlt : " CIH Logo"
+        },
+
+    },
+    productsAndServices : [
+        {
+            name : "Engineering Management Contracting",
+            href : "/products-and-services/engineering-management-contracting",
+            img : "/images/sc1.webp",
+            description : "Engineering Construction Management",
+            details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        },
+        {
+            name : "Engineering Procurement & Construction",
+            href : "/products-and-services/engineering-procurement-construction",
+            img : "/images/sc2.webp",
+            description : "Customer-centric quality construction",
+            details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        },
+        {
+            name : "Chemical Supplies",
+            href : "/products-and-services/chemical-supplies",
+            img : "/images/sc3.webp",
+            description : "Your way to Chemical solutions",
+            details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        },
+        {
+            name : "Refinery Products",
+            href : "/products-and-services/refinery-products",
+            img : "/images/sc4.webp",
+            description : "Supply of fuels and fuel additives",
+            details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        },
+        {
+            name : "Well Testing Equiments & Services",
+            href : "/products-and-services/well-testing-equipments-services ",
+            img : "/images/sc5.webp",
+            description : "For any types of Oil wells",
+            details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        },
+        {
+            name : "Metal Spray Coating",
+            href : "/products-and-services/metal-spray-coating",
+            img : "/images/sc6.webp",
+            description : "To avoid corrosion and enhance product life",
+            details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        }
+    ],
+    contact : {
+        domainName : "mtemp.smartoreach.com",
+        phone : "+7 7018050311",
+        whatsapp : "https://wa.link/7pd8n0",
+        mailID : "info@cih.kz",
+        googleSheetUrl : "https://script.google.com/macros/s/AKfycbyuMj5UxnGLV2nu4KGpjDMG4choZcN0mSa5KCYcb3q2w9tk5T27blaz/exec",
+        address : [
+            {
+                line : "Caspian Investment Holding,"
+            },
+            {
+                line : "Door #3,4, Street Name,"
+            },
+            {
+                line : "Area Name, City,"
+            },
+            {
+                line : "State, Country - xxxxxx "
+            }
+        ],
+        googleMap : "https://goo.gl/maps/aK2oVFGdkm7B2H9f9",
+        googleMapEmbed : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15669.488002926582!2d79.84087805!3d10.9352467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5511338921f131%3A0x7f52220a83d52504!2sPSR%20Cinema%20Theatre!5e0!3m2!1sen!2sin!4v1613235856228!5m2!1sen!2sin"
+
+    },
+    footer : {
+        socialMedia : [
+            {
+                name : "Map",
+                img : "/images/gmaps.png",
+                link : "https://goo.gl/maps/aK2oVFGdkm7B2H9f9"
+            },
+            {
+                name : "Email",
+                img : "/images/gmail.png",
+                link : "mailto:info@cih.kz"
+            },
+        ]
+    }
+
+}
